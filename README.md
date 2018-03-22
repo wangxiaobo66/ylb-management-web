935ab48ec484211d2faf35077e31beaa40b6e19f
wangxiaobo <457117157@qq.com>
2018-03-22 15:22:07 +0800

    commit

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 前端框架 react+redux
 工程化工具 gulp
-模块打包 browserify （再做考虑。。）
-入口文件管理工具（代码编译合并） webpack
+模块打包 browserify （正在做。。）
 第三方 bootstrap
--- gulp/js.js
+++ gulp/js.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
+const browserify = require('browserify');
+const babelify = require('babelify');
+const reactify = require('reactify');
+const source = require('vinyl-source-stream');
 
-gulp.task('js', function() {
+gulp.task('devJs', () => {
+    /*
+        gulp.src('./static/js/pages/index.js')
+            .pipe(babel({
+                presets: ['env', 'react'],
+                plugins: ['transform-runtime']
+            }))
+            .pipe(gulp.dest('dist/js'))
+            */
 
-    gulp.src('./static/js/pages/index.js')
-        .pipe(babel({
-            presets: ['env', 'react'],
-            plugins: ['transform-runtime']
-        }))
-        .pipe(gulp.dest('dist/js'))
+    return browserify('./static/js/pages/index.js')
+        .transform(reactify)
+        //.transform(babelify,{presets: ["es2015","react"]})
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('./dist/js'))
 });
